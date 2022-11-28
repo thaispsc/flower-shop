f6154106294aaa85fb06a6848ca9458d7498057e
thaispsc <thaispcavalcante@gmail.com>
2022-11-27 22:59:14 -0300

    Best sellers component

---
+++ src/flower-shop/ui/best-sellers/BestSellers.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material'
+
+export const BestSellers = () => {
+  return (
+    <Box marginTop={14.5}>
+      <Typography variant='h2' textAlign='center'>
+        Best Sellers
+      </Typography>
+    </Box>
+  )
+}
