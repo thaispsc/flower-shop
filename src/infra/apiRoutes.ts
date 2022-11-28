9aa5399a340ef7ac1fd977e51b943686d4bc87a5
thaispsc <thaispcavalcante@gmail.com>
2022-11-27 22:55:01 -0300

    adding route

--- src/infra/apiRoutes.ts
+++ src/infra/apiRoutes.ts
@@ -2,3 +2,5 @@ import { FLOWER_SHOP_URL } from '../lib/constants/enviroments'
 
 export const GET_FLOWER_API_ENDPOINT = (flowerId: number) =>
   `${FLOWER_SHOP_URL}/${flowerId}`
+
+export const GET_ALL_FLOWERS_API_ENDPOINT = () => `${FLOWER_SHOP_URL}`
