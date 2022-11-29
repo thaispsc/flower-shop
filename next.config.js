f974dbb2b6e6dbd5bd56bc69dec5de7777495277
thaispsc <thaispcavalcante@gmail.com>
2022-11-29 09:27:54 -0300

    settig images remote patterns

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
