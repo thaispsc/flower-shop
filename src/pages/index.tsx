dd80ac07c7c59be951d98f26af628a2286fb0bc0
thaispsc <thaispcavalcante@gmail.com>
2022-12-03 15:35:19 -0300

    initial page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  )
+}
