cb740af2023b6f2be37942740dd671b19e7df03d
thaispsc <thaispcavalcante@gmail.com>
2022-12-03 12:23:02 -0300

    flower fixture

---
+++ tests/__fixtures__/flowerFixture.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker'
+
+function generateDefaultFlowerParams(): Flower {
+  return {
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    image: faker.internet.url(),
+    price: Number(faker.commerce.price()),
+  }
+}
+
+export default class FlowerFixture {
+  private flower: Flower
+
+  constructor(flowerParams: Partial<Flower> = {}) {
+    this.flower = { ...generateDefaultFlowerParams(), ...flowerParams }
+  }
+
+  build() {
+    return this.flower
+  }
+}
