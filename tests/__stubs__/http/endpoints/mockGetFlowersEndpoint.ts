7954938748d1693bb3005f73a49ed7142d65cac0
thaispsc <thaispcavalcante@gmail.com>
2022-12-06 17:31:49 -0300

    mocking get flowers function

--- tests/__stubs__/http/endpoints/mockGetFlowersEndpoint.ts
+++ tests/__stubs__/http/endpoints/mockGetFlowersEndpoint.ts
@@ -1,39 +1,31 @@
 import { rest } from 'msw'
+import FlowerFixture from '../../../__fixtures__/flowerFixture'
+import { DataFound, DataNotFound } from './types'
 
-export const mockGetFlowersEndpoint = () => ({
+type MockProps = DataFound | DataNotFound
+
+export const mockGetFlowersEndpoint = ({ status, data }: MockProps) => ({
+  ...data,
   request: rest.get('http://localhost:3333/flowers', (_req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          name: 'Daisy',
-          image:
-            'https://images.pexels.com/photos/11420027/pexels-photo-11420027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          price: '5,00',
-        },
-        {
-          id: 2,
-          name: 'Sun Flower',
-          image:
-            'https://images.pexels.com/photos/5551654/pexels-photo-5551654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          price: '3,00',
-        },
-        {
-          id: 3,
-          name: 'White Rose',
-          image:
-            'https://images.pexels.com/photos/8569653/pexels-photo-8569653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          price: '4,50',
-        },
-        {
-          id: 4,
-          name: 'Orchild',
-          image:
-            'https://images.pexels.com/photos/12573405/pexels-photo-12573405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          price: '3,75',
-        },
-      ]),
-    )
+    return res(ctx.status(status), ctx.json(data))
   }),
+})
+
+export const buildFlowersListFoundResponse = (
+  flowersList: Flower[] = [
+    new FlowerFixture().build(),
+    new FlowerFixture().build(),
+    new FlowerFixture().build(),
+    new FlowerFixture().build(),
+  ],
+) => ({
+  status: 200,
+  data: flowersList,
+})
+
+export const buildFlowersListNotFoundResponse = () => ({
+  status: 404,
+  data: {
+    error: 'Flowers list not found',
+  },
 })
