93f50ed4011f09791d70be1ce287d6a58520e075
thaispsc <thaispcavalcante@gmail.com>
2022-12-13 13:11:20 -0300

    border radius in card and image

--- src/flower-shop/ui/flower-card/FlowerCard.tsx
+++ src/flower-shop/ui/flower-card/FlowerCard.tsx
@@ -9,8 +9,17 @@ interface FlowerCardProps {
 export const FlowerCard = ({ flower }: FlowerCardProps) => {
   return (
     <Link href={`/flowers/${flower.id}`} underline='none'>
-      <Card key={flower.id} sx={{ maxWidth: 287, padding: '15px' }}>
-        <Image src={flower.image} alt={'Flower'} width={240} height={240} />
+      <Card
+        key={flower.id}
+        sx={{ maxWidth: 287, padding: '15px', borderRadius: '5px' }}
+      >
+        <Image
+          src={flower.image}
+          alt={'Flower'}
+          width={240}
+          height={240}
+          style={{ borderRadius: '5px' }}
+        />
 
         <CardContent sx={{ padding: '15px 0 0 0' }}>
           <Typography gutterBottom variant='h5' component='div'>
