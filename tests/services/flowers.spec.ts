93c2942e29489c712311bbaddc38b2b14ae3d3ca
thaispsc <thaispcavalcante@gmail.com>
2022-12-21 00:26:20 -0300

    fixing test

--- tests/services/flowers.spec.ts
+++ tests/services/flowers.spec.ts
@@ -1,5 +1,10 @@
-import { getFlowerById, getFlowers } from '../../src/services/flowers'
+import { getFlowerById, getAllFlowers } from '../../src/services/flowers'
 import FlowerFixture from '../__fixtures__/flowerFixture'
+import FlowersListFixture from '../__fixtures__/flowersListFixture'
+import {
+  buildFlowersListFoundResponse,
+  mockGetAllFlowersEndpoint,
+} from '../__stubs__/http/endpoints/mockGetAllFlowersEndpoint'
 import {
   buildFlowerFoundResponse,
   mockGetFlowerByIdEndpoint,
@@ -8,23 +13,31 @@ import { mockServer } from '../__stubs__/http/mockServer'
 
 describe('getFlowers', () => {
   it('should return the list of flowers', async () => {
-    const flowersList = await getFlowers()
+    const flowersListVariant = new FlowersListFixture().build()
+    const getFlowerListOKResponse =
+      buildFlowersListFoundResponse(flowersListVariant)
+    const { request: mockedGetFlowersListEndpoint } = mockGetAllFlowersEndpoint(
+      getFlowerListOKResponse,
+    )
+    mockServer.use(...[mockedGetFlowersListEndpoint])
 
+    const flowersList = await getAllFlowers()
     expect(flowersList).toBeDefined()
+    expect(flowersList).toEqual(flowersListVariant)
   })
 })
 
 describe('getFlowerById', () => {
-  const flowerVariant = new FlowerFixture().build()
-
-  const getFlowerOKResponse = buildFlowerFoundResponse(flowerVariant)
-  const { request: mockedGetFlowerByIdEndpoint } = mockGetFlowerByIdEndpoint(
-    flowerVariant.id,
-    getFlowerOKResponse,
-  )
-  mockServer.use(...[mockedGetFlowerByIdEndpoint])
-
   it('should return a flower', async () => {
+    const flowerVariant = new FlowerFixture().build()
+
+    const getFlowerOKResponse = buildFlowerFoundResponse(flowerVariant)
+    const { request: mockedGetFlowerByIdEndpoint } = mockGetFlowerByIdEndpoint(
+      flowerVariant.id,
+      getFlowerOKResponse,
+    )
+    mockServer.use(...[mockedGetFlowerByIdEndpoint])
+
     const flower = await getFlowerById(flowerVariant.id)
 
     expect(flower).toBeDefined()
