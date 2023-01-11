636b3b663ab61f5a098343a941cced7274c94fe6
thaispsc <thaispcavalcante@gmail.com>
2023-01-10 23:27:40 -0300

    grid items

--- src/flower-shop/pages/FlowerIdPage.tsx
+++ src/flower-shop/pages/FlowerIdPage.tsx
@@ -1,20 +1,29 @@
-import { Container } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import React from 'react'
 import FlowerDescriptionCard from '../ui/flower-description-card/FlowerDescriptionCard'
 import Header from '../ui/header/Header'
+import { RelatedItemsCard } from '../ui/related-items-card/RelatedItemsCard'
 import { ReviewCard } from '../ui/review-card/ReviewCard'
 
 interface FlowerIdPageProps {
   flower: Flower
+  flowers: Flower[]
 }
 
-const FlowerIdPage = ({ flower }: FlowerIdPageProps) => {
+const FlowerIdPage = ({ flower, flowers }: FlowerIdPageProps) => {
   return (
     <>
       <Header />
       <Container maxWidth='lg' sx={{ marginTop: '175px' }}>
         <FlowerDescriptionCard flower={flower} />
-        <ReviewCard flower={flower} />
+        <Grid container spacing={5}>
+          <Grid item md={6}>
+            <ReviewCard flower={flower} />
+          </Grid>
+          <Grid item md={6}>
+            <RelatedItemsCard flowers={flowers} />
+          </Grid>
+        </Grid>
       </Container>
     </>
   )
