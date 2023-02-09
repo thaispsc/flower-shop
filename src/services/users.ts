5aa73e8cb8398e0e32c059253af34b6fe0aba6ce
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-02-09 16:23:27 -0300

    adding the request to create user

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,1 +1,11 @@
 import { api } from './config'
+
+export const CreateUser = async (user: Omit<User, 'id'>) => {
+  try {
+    const response = await api.post('/users', user)
+    return response.data
+  } catch (error) {
+    const err = new Error('Failed to create new user')
+    throw err
+  }
+}
