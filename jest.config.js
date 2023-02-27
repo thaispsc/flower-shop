c696f3173cd9a5219e93e0155ee92c82ee634db6
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-02-27 17:40:25 -0300

    updating jest configs

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,32 @@ const customJestConfig = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  clearMocks: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tests/**/?(*.)+(spec|test).(ts|tsx)'],
+  transform: {
+    '\\.(ts|tsx|js|jsx)?$': 'ts-jest',
+  },
+  setupFiles: ['<rootDir>/jest.setup.tests.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/tests/__mocks__/fileMock.ts',
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
