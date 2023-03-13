fe3fbc506d56bbb7132b725079638067c81b3d42
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-12 21:57:22 -0300

    login user test

---
+++ tests/ui/LoginComponent.spec.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import LoginComponent from '../../src/flower-shop/ui/login/LoginComponent'
+import { loginUser } from '../../src/services/users'
+
+jest.mock('../../src/services/users', () => ({
+  loginUser: jest.fn(),
+}))
+
+describe('<LoginComponent>', () => {
+  const typeUsername = async (username: string) => {
+    const usernameInput = screen.getByPlaceholderText('Username')
+    await userEvent.type(usernameInput, username)
+  }
+
+  const typePassword = async (password: string) => {
+    const passwordInput = screen.getByPlaceholderText('Password')
+    await userEvent.type(passwordInput, password)
+  }
+
+  const submitLoginForm = async () => {
+    const loginButton = screen.queryByRole('button', { name: /login/i })
+    await userEvent.click(loginButton)
+  }
+
+  it('should login an user when the login button is clicked', async () => {
+    render(<LoginComponent />)
+
+    const username = 'thaispc'
+    const password = '123456'
+
+    await typeUsername(username)
+    await typePassword(password)
+
+    await submitLoginForm()
+
+    expect(loginUser).toHaveBeenCalledWith({ username, password })
+  })
+})
