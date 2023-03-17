dd8197b69bd42e51a2cf07011bc27d21d638dee0
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-17 18:06:25 -0300

    creating getUser function

--- src/services/users.ts
+++ src/services/users.ts
@@ -12,17 +12,29 @@ export const createUser = async (user: Omit<User, 'id'>) => {
 
 export const loginUser = async (user: Partial<User>) => {
   try {
-    const existingUser = await api.get(
-      `/users?username=${user.username}&password=${user.password}`,
-    )
+    // const existingUser = await api.get(
+    //   `/users?username=${user.username}&password=${user.password}`,
+    // )
 
-    if (existingUser.data.length === 0) {
-      throw new Error('Invalid username or password')
-    }
+    // if (existingUser.data.length === 0) {
+    //   throw new Error('Invalid username or password')
+    // }
     const response = await api.post('/login', user)
     return response.data
   } catch (error) {
     const err = new Error('Failed to log user')
+    throw err
+  }
+}
+
+export const getUser = async (user: Partial<User>) => {
+  try {
+    const response = await api.get(
+      `/users?username=${user.username}&password=${user.password}`,
+    )
+    return response.data
+  } catch (error) {
+    const err = new Error('Failed to get user')
     throw err
   }
 }
