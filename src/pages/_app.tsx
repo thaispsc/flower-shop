c2ba87ada2efbb37c7a5ae00260a6837bb134079
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-17 15:51:01 -0300

    adding AuthProvider to flower shop pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import { GlobalStyles, ThemeProvider } from '@mui/material'
 import theme from '../lib/theme'
 import 'styles/fonts/roboto/roboto.css'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles
-        styles={{
-          body: { backgroundColor: '#F2F2F2', margin: 0 },
-        }}
-      />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles
+          styles={{
+            body: { backgroundColor: '#F2F2F2', margin: 0 },
+          }}
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
 
