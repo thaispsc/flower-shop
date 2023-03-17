86dfd6564d7c96c6816b08e6e7c4c6aa489aacaf
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-17 18:07:00 -0300

    removing loginUser service

--- src/services/users.ts
+++ src/services/users.ts
@@ -10,23 +10,6 @@ export const createUser = async (user: Omit<User, 'id'>) => {
   }
 }
 
-export const loginUser = async (user: Partial<User>) => {
-  try {
-    // const existingUser = await api.get(
-    //   `/users?username=${user.username}&password=${user.password}`,
-    // )
-
-    // if (existingUser.data.length === 0) {
-    //   throw new Error('Invalid username or password')
-    // }
-    const response = await api.post('/login', user)
-    return response.data
-  } catch (error) {
-    const err = new Error('Failed to log user')
-    throw err
-  }
-}
-
 export const getUser = async (user: Partial<User>) => {
   try {
     const response = await api.get(
